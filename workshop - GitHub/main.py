c8886e70d94085b85d454ae087b4af86ad9d7d1a
Bruno-Calazans <brccordeiro@gmail.com>
2023-06-06 20:49:03 -0300

    inicia o programa e solicita os dados do usuario

---
+++ workshop - GitHub/main.py
@@ -0,0 +1,8 @@
+print()
+name = input('Digite seu nome: ')
+city = input('Digite sua cidade: ')
+gender = input('Digite seu sexo: ')
+print()
+print('Seus dados são: ')
+print(name,' - ',city,' - ',gender)
+print()
